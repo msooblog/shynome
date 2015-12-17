d8769e6b4b86a688e9973c7f07579ecec859a842
shynome <msooblog@outlook.com>
2015-12-17 23:57:34 +0800

    Revert "Revert "更好的传输数据""
    This reverts commit ab5102db96724b9d1fa8f431155b8ee1f4406c6a.

--- node/webSocket/toWs.js
+++ node/webSocket/toWs.js
@@ -1,11 +1,11 @@
 'use strict'
 let	log		=	(...rest) => console.log(...rest)
-let toWs = data => {
+let toWs = (data,FIN,Opcode) => {
+	if(typeof FIN !== 'number')FIN = 1;
+	if(typeof Opcode !== 'number')Opcode = 1;
 	let e = [],d=new Buffer(data)
-	e[0]=(1<<7)+(1)
+	e[0]=(FIN<<7)+(Opcode)
 	e[1]= ( d.length<126 && d.length )
 	return Buffer.concat([new Buffer(e),d])
 }
-
-
 module.exports = toWs
