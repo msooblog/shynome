d9cd5c2435954b8f10e2be3fc86829c66c885ada
shynome <msooblog@outlook.com>
2016-01-09 15:50:26 +0800

    遍历对象错了

--- tpl/tpl.js
+++ tpl/tpl.js
@@ -1,7 +1,7 @@
 'use strict'
 module['exports']=(..._scope)=>{
 	_scope={s:_scope[0],o:_scope[1],a:'',v:''}
-	for(let i in _scope.v){_scope.a+=`${i},`;_scope.v+=`_scope.o['${i}'],`}
+	for(let i in _scope.o){_scope.a+=`${i},`;_scope.v+=`_scope.o['${i}'],`}
 	try{
 		return eval(`((${_scope.a.slice(0,-1)})=>${_scope.s})(${_scope.v.slice(0,-1)})`)
 	}catch(err){
