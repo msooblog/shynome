3f0f2c3fdda80e29b6a3da524cd893a8c4944359
shynome <msooblog@outlook.com>
2016-01-09 13:19:51 +0800

    $scope
    只声明了一个变量$scope

--- tpl/tpl.js
+++ tpl/tpl.js
@@ -1,13 +1,13 @@
 'use strict'
 {
-let tpl=(str,o)=>{
-	o={o:o,arr:'',obj:''}
-	for(let i in o.o){
-		o.arr+=`${i},`
-		o.obj+=`o.o['${i}'],`
+let tpl=(s,o)=>{
+	this.a=this.o=''
+	for(let i in o){
+		this.a+=`${i},`
+		this.o+=`o['${i}'],`
 	}
 	{
-		return eval(`((${o.arr.slice(0,-1)})=>${str})(${o.obj.slice(0,-1)})`)
+		return eval(`((${this.a.slice(0,-1)})=>${s})(${this.o.slice(0,-1)})`)
 	}
 }
 module['exports']=tpl
